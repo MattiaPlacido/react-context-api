524576ac8dbc9949c9fdea03e451ede3dbf00e59
MattiaPlacido <placido.mattia@gmail.com>
2024-12-13 16:29:52 +0100

    Rimosso debugging da aboutpage

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,11 +1,7 @@
-import { usePosts } from "../contexts/PostsContext";
-
 export default function AboutPage() {
-  const { posts } = usePosts();
   return (
     <main className="container py-5 text-center">
       <h2>About</h2>
-      <div>{posts.map((post) => post.title)}</div>
     </main>
   );
 }
